fedb3a0b3ee0a3629c0a365c95fb4876827d3910
Vinicius Patzer <viniciuspatzer2@gmail.com>
2021-08-08 01:56:18 -0300

    new commit

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ const jokeEl = document.querySelector('#joke_text');
 const buttonEl = document.querySelector('#button');
 
 let jokesArr = [];
-let jokesRep = [];
+let jokesRep = [`Java and C were telling jokes. It was C's turn, so he writes something on the wall, points to it and says "Do you get the reference?" But Java didn't.`];
 
 
 const fillJokes = async function () {
